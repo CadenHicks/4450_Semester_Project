7552a7e1a82236c6529f188fa8d4bfa0e56cfd14
CadenHicks <cadenhicks@gmail.com>
2022-11-08 19:45:37 -0600

    Added Printer Class and Changed Program CS to run the generated code.

--- AntlrCSharp/Program.cs
+++ AntlrCSharp/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using System;
+using System.Text;
+
+namespace AntlrCSharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string input = "";
+                StringBuilder text = new StringBuilder();
+                Console.WriteLine("Input the chat.");
+
+                // to type the EOF character and end the input: use CTRL+D, then press <enter>
+                while ((input = Console.ReadLine()) != "u0004")
+                {
+                    text.AppendLine(input);
+                }
+
+                AntlrInputStream inputStream = new AntlrInputStream(text.ToString());
+                FinalLexer Lexer = new FinalLexer(inputStream);
+                CommonTokenStream tokens = new CommonTokenStream(Lexer);
+                FinalParser parser = new FinalParser(tokens);
+                parser.BuildParseTree = true;
+                IParseTree tree = parser.prog();
+                Printer printer = new Printer();
+                ParseTreeWalker.Default.Walk(printer, tree);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+        }
+    }
+}
+
